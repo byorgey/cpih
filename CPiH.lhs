5e470c0411ddc8af3f8d23f5a2e5c4f5fe53d609
Brent Yorgey <byorgey@gmail.com>
2021-08-20 11:10:42 -0500

    basic skeleton

---
+++ CPiH.lhs
@@ -0,0 +1,12 @@
+% -*- mode: LaTeX; compile-command: "stack runhaskell --package shake -- Shake.hs" -*-
+
+\documentclass{book}
+
+\title{Competitive Programming in Haskell}
+\author{Brent A. Yorgey}
+
+\begin{document}
+
+\maketitle
+
+\end{document}
---
+++ Shake.hs
@@ -0,0 +1,25 @@
+import           Development.Shake
+import           Development.Shake.FilePath
+import           System.Exit
+
+lhs2TeX, pdflatex, rubber, agda, bibtex :: String
+lhs2TeX  = "lhs2TeX"
+pdflatex = "pdflatex"
+rubber   = "rubber"
+agda     = "agda"
+bibtex   = "bibtex"
+
+main :: IO ()
+main = shake shakeOptions $ do
+
+  want ["CPiH.pdf"]
+
+  "*.tex" %> \output -> do
+      let input = output -<.> "lhs"
+      need [input]
+      cmd lhs2TeX ["-o", output, input]
+
+  "*.pdf" %> \output -> do
+      let input = output -<.> "tex"
+      need [input]
+      cmd rubber ["-d", "--unsafe", input]
