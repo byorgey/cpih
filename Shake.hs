c4596d01700c16996e9e007c1e94c63b04a13538
Brent Yorgey <byorgey@gmail.com>
2023-02-14 09:57:39 -0600

    move away from using lhs2TeX

--- Shake.hs
+++ Shake.hs
@@ -2,22 +2,14 @@ import           Development.Shake
 import           Development.Shake.FilePath
 import           System.Exit
 
-lhs2TeX, pdflatex, rubber, agda, bibtex :: String
-lhs2TeX  = "lhs2TeX"
+pdflatex, rubber :: String
 pdflatex = "pdflatex"
 rubber   = "rubber"
-agda     = "agda"
-bibtex   = "bibtex"
 
 main :: IO ()
 main = shake shakeOptions $ do
 
   want ["CPiH.pdf"]
-
-  "*.tex" %> \output -> do
-      let input = output -<.> "lhs"
-      need [input]
-      cmd lhs2TeX ["--tt", "-o", output, input]
 
   "*.pdf" %> \output -> do
       let input = output -<.> "tex"
